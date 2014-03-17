eb40024d4c65855b45bf7577e68484744dd8ce83
Luka Rajcevic <traxdater12@gmail.com>
2014-03-17 11:39:34 +0100

    Code cleanup, fixed email sending (made transactional)

--- app/code/community/Inchoo/SupportTicket/Model/Observer.php
+++ app/code/community/Inchoo/SupportTicket/Model/Observer.php
@@ -8,14 +8,26 @@ class Inchoo_SupportTicket_Model_Observer
 {
     public function sendMailToAdmin($observer) {
         if (Mage::getStoreConfig('sales_email/order/enabled') == 1) {
+
+            $customer = $observer->getEvent()->getCustomer();
+            $data = $observer->getEvent()->getData();
+
             $senderName = Mage::getStoreConfig('sales_email/order/identity');
             $adminMail = Mage::getModel('admin/user')->load(2)->getEmail();
             $sender = Array('name' => $senderName,
                             'email' => Mage::getStoreConfig('trans_email/ident_general/email'));
             $templateId = Mage::getStoreConfig('sales_email/order/template');
+            // $emailTemplate = Mage::getModel('core/email_template')->loadDefault('inchoo_supportticket_email_template');
+            $url = Mage::helper('adminhtml')->getUrl("adminhtml/inchoo_supportticket/index");
             $store = Mage::app()->getStore();
-            $vars = array('customer' => $customer);
+            $vars = array('customername' => $customer['firstname']." ".$customer['lastname'],
+                          'adminName' => 'Administrator',
+                          'ticketId' => $data['data']['ticket_id'],
+                          'ticketSubject' => $data['data']['subject'],
+                          'url' => $url
+                          );
             Mage::getModel('core/email_template')->sendTransactional($templateId,
+                                                                     // $emailTemplate,
                                                                      $sender,
                                                                      $adminMail,
                                                                      'Admin',
@@ -23,16 +35,5 @@ class Inchoo_SupportTicket_Model_Observer
                                                                      $store->getId());
         }
 
-        // if (Mage::getStoreConfig('system/smtp/ticket_mail') == 1) {
-        //     $customer = $observer->getEvent()->getCustomer();
-        //     $data = $observer->getEvent()->getData();
-
-        //     $adminMail = Mage::getModel('admin/user')->load(2)->getEmail();
-        //     // $emailTemplate = Mage::getModel('core/email_template')->loadDefault('inchoo_supportticket_email_template');
-        //     // $emailTemplate->setSenderEmail(Mage::getStoreConfig('trans_email/ident_general/email', Mage::app()->getStore()->getStoreId()));
-        //     // $emailTemplate->setSenderName('System');
-        //     // $emailTemplate->setTemplateSubject('New Ticket');
-        //     // $emailTemplate->send($adminMail, "Admin", $vars);
-        // }
     }
 }
